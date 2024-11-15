d69d60838b42fa7c155ee3f9b1942bd95350b0eb
Julian Thomas <87448079+00Julian00@users.noreply.github.com>
2024-11-15 17:45:09 +0100

    Moved tool logic to a seperate file

--- app/llm_manager.py
+++ app/llm_manager.py
@@ -9,68 +9,7 @@ Description: This script manages interactions with LLMs.
 import groq
 
 from .security import KeyManager
-
-class LLMToolParameter:
-    def __init__(
-                self,
-                name: str,
-                description: str,
-                type: str,
-                required: bool
-                ) -> None:
-
-        """
-        Defines a parameter for a tool.
-        """
-
-        self.name = name
-        self.description = description
-        self.type = type
-        self.required = required
-
-class LLMTool:
-    def __init__(
-                self,
-                name: str,
-                description: str,
-                parameters: list[LLMToolParameter]
-                ) -> None:
-
-        """
-        Defines a tool that can be used by the LLM.
-        """
-
-        self.name = name
-        self.description = description
-        self.parameters = parameters
-
-class LLMToolCallParameter:
-    def __init__(
-                self,
-                name: str,
-                value: str #The value is always a string. Casting needs to be handled by the tool that is executed. Alternativly leave the type ambigous and look up the type in the tool's parameter definition.
-                ) -> None:
-
-        """
-        Defines a parameter for a tool call.
-        """
-
-        self.name = name
-        self.value = value
-
-class LLMToolCall:
-    def __init__(
-                self,
-                name: str,
-                parameters: list[LLMToolCallParameter]
-                ) -> None:
-
-        """
-        Defines a tool call made by the LLM.
-        """
-
-        self.name = name
-        self.parameters = parameters
+from .tool_manager import LLMTool, LLMToolCall
 
 class LLMResponse:
     def __init__(
